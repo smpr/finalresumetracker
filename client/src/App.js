87951d68bfa3daf71ad08f96c05b77cd874870ee
smpr <gcbrown1116@gmail.com>
2017-12-20 23:23:02 -0600

    authorize setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { clearAuthTokens, saveAuthTokens, setAxiosDefaults, userIsLoggedIn } from "./util/SessionHeaderUtil"
 
 class App extends Component {
+  state = {
+    redirectToLogin: false,
+    signedIn: false,
+
+  }
+  async componentWillMount() {
+    try {
+      const signedIn = userIsLoggedIn()
+
+      let categories = []
+      if (signedIn) {
+        setAxiosDefaults()
+        categories = await this.getCategories()
+      }
+
+      this.setState({
+        categories,
+        signedIn
+
+
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  //this is devise sign up that will be passed down via props
+  signUp = async (email, password, password_confirmation) => {
+    try {
+      const payload = {
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation
+      }
+      const response = await axios.post('/auth', payload)
+
+      saveAuthTokens(response.headers)
+
+      this.setState({
+        signedIn: true,
+      })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  signIn = async (email, password) => {
+    try {
+      const payload = {
+        email,
+        password
+      }
+      const response = await axios.post('/auth/sign_in', payload)
+      saveAuthTokens(response.headers)
+      this.setState({
+        signedIn: true,
+
+      })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  signOut = async (event) => {
+    try {
+      event.preventDefault()
+
+      await axios.delete('/auth/sign_out')
+
+      clearAuthTokens();
+
+      this.setState({ signedIn: false })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+<div>
+  </div>
     );
   }
 }
