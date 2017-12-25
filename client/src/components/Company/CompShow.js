dd1c8193547e353b8f8f5e2ea2e51e19048b79f3
smpr <gcbrown1116@gmail.com>
2017-12-25 14:36:55 -0600

    comp show now showing all company info

--- client/src/components/Company/CompShow.js
+++ client/src/components/Company/CompShow.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Container, FormContainer, BodyContainer, Style, TextLabelStyle } from "../StyledComponents/DefaultStyle"
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class CompShow extends Component {
+    state = {
+        company: {}
+    }
+    async componentWillMount() {
+        try {
+            const compId = this.props.match.params.compId
+            const company = await axios.get(`/api/companies/${compId}`)
+            this.setState({ company: company.data })
+            console.log(this.state.company)
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
     render() {
         return (
-            <div>
-                Company Show for ID {this.props.match.params.compId}
-            </div>
+            <BodyContainer>
+                <Container>
+                    <FormContainer>
+                        <div>
+                            <b>Company Name: </b>{this.state.company.name}
+                        </div>
+                        <div>
+                            <b>Phone Number: </b>{this.state.company.phone}
+                        </div>
+                        <div>
+                            <b>Industry: </b>{this.state.company.industry}
+                        </div>
+                        <div>
+                            <b>Address: </b>{this.state.company.address}
+                        </div>
+                        <div>
+                            <b>City: </b>{this.state.company.city}
+                        </div>
+                        <div>
+                            <b>State: </b>{this.state.company.state}
+                        </div>
+                        <div>
+                            <b>Zip: </b>{this.state.company.zip}
+                        </div>
+                        
+                    </FormContainer>
+                </Container>
+            </BodyContainer>
         );
     }
 }
